7ccc0c40f88fd8623a44bf9aebcae9228c805d85
Winson Ma <winsonma2002@gmail.com>
2025-02-19 16:17:20 +0000

    Added all the necessary files and started on the structure of each file

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,11 @@
 package Controller;
 
+import Model.Account;
+import Model.Message;
+//import Service.AccountService;
+//import Service.AccountService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -17,8 +23,49 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
+        app.post("localhost:8080/register", this::register);
+        app.post("localhost:8080/login", this::login);
+        app.post("localhost:8080/messages", this::createMessage);
+        app.get("localhost:8080/messages", this::getAllMessage);
+        app.get("localhost:8080/messages/{message_id}", this::getMessageWithID);
+        app.delete("localhost:8080/messages/{message_id}", this::deleteMessageWithID);
+        app.patch("localhost:8080/messages/{message_id}", this::updateMessageWithId);
+        app.get("localhost:8080/accounts/{account_id}/messages", this::getMessageWithAccountID);
 
         return app;
+    }
+
+    private void register(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+    }
+
+    private void login(Context ctx) {
+
+    }
+
+    private void createMessage(Context ctx) {
+
+    }
+
+    private void getAllMessage(Context ctx) {
+
+    }
+
+    private void getMessageWithID(Context ctx) {
+
+    }
+
+    private void deleteMessageWithID(Context ctx) {
+
+    }
+
+    private void updateMessageWithId(Context ctx) {
+
+    }
+
+    private void getMessageWithAccountID(Context ctx) {
+
     }
 
     /**
